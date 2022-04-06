7832bc8f4981c4fe0cd6632386d91f795e3caa38
Antonioz9 <antoniozurita99@gmail.com>
2022-04-06 13:44:16 +0200

    Se ha creado el mapa, además se ha incluido el certificado SSL para HTTPS en el servidor y se ha registrado un dominio dinamico

--- Emergencias/src/app/app.component.ts
+++ Emergencias/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MapInfoWindow, MapMarker} from '@angular/google-maps';
+
+const centeromapa1: google.maps.LatLngLiteral = {lat: 30, lng: -110};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
   title = 'Emergencias';
+  public markers: any[];
+  public zoom: number;
+  public centromapa: any;
+
+
+
+  constructor() {
+    this.markers = [];
+    this.zoom = 5;
+    this.centromapa = centeromapa1;
+    
+  }
+
+  
+
+  ngOnInit() {
+       
+    this.markers.push({
+      
+      position: {
+        lat: 40.4381311,
+        lng: -3.8196233
+      },
+      label: {
+        color: "black",
+        text: "Madrid"
+      }
+    });
+ 
+    this.markers.push({
+      
+      position: {
+        lat: 36.72314249921822, lng: -4.421610174606653
+      },
+      label: {
+        color: "black",
+        text: "Málaga"
+      }
+    });
+  }
+
 }
