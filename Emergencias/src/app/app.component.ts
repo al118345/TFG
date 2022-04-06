822c7a46d691947b074e1c6d39f461abd2fa6596
Antonioz9 <antoniozurita99@gmail.com>
2022-04-06 13:50:18 +0200

    Arreglado fallo mapa, está centrado se muestra

--- Emergencias/src/app/app.component.ts
+++ Emergencias/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
-const centeromapa1: google.maps.LatLngLiteral = {lat: 30, lng: -110};
+const centeromapa1: google.maps.LatLngLiteral = {lat: 40.4381311, lng: -3.8196233};
 
 @Component({
   selector: 'app-root',
