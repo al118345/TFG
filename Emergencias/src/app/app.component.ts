6cdb135bf7f7e209d88a846628ceca1ad9ee4c67
Antonioz9 <antoniozurita99@gmail.com>
2022-04-25 19:01:49 +0200

    Agregado Open Street Map

--- Emergencias/src/app/app.component.ts
+++ Emergencias/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {MapInfoWindow, MapMarker} from '@angular/google-maps';
+//import {MapInfoWindow, MapMarker} from '@angular/google-maps';
 
 
-const centeromapa1: google.maps.LatLngLiteral = {lat: 40.4381311, lng: -3.8196233};
+//const centeromapa1: google.maps.LatLngLiteral = {lat: 40.4381311, lng: -3.8196233};
 
 @Component({
   selector: 'app-root',
--- Emergencias/src/app/mapa/mapa.component.ts
+++ Emergencias/src/app/mapa/mapa.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+
+
+/*import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { MapInfoWindow, MapMarker } from "@angular/google-maps";
 import { GoogleMap } from '@angular/google-maps';
 /*
@@ -27,7 +29,7 @@ const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
 
  
-*/
+*//*
 
 @Component({
   selector: 'app-mapa',
@@ -107,32 +109,45 @@ export class MapaComponent implements OnInit {
       }
   });
     
-  }
-  /*
-  private map:any;
-  @Input() lat: number = DEFAULT_LAT;
-  @Input() lon: number = DEFAULT_LON;
-  @Input() titulo: string = TITULO ;
- 
-  constructor() {
-  }
- 
-  ngOnInit(): void {
-    this.initMap();
-  }
- 
- 
- 
-  private initMap(): void {
-      //configuración del mapa
-      this.map = L.map('map', {
-        center: [this.lat, this.lon],
-        attributionControl: false,
-        zoom: 14
-      });
- 
-      //iconos personalizados
-      var iconDefault = L.icon({
+  }*/
+  import {LeafletService} from "src/app/service/leaflet.service";
+
+  import { Component, Input, OnInit } from '@angular/core';
+
+
+
+  export const DEFAULT_LAT = 48.20807;
+  export const DEFAULT_LON =  16.37320;
+  export const TITULO = 'Emergencias';
+   
+  
+  @Component({
+    selector: 'app-mapa',
+    templateUrl: './mapa.component.html',
+    styleUrls: ['./mapa.component.css']
+  })
+  export class MapaComponent implements OnInit {
+    private map:any;
+    @Input() lat: number = DEFAULT_LAT;
+    @Input() lon: number = DEFAULT_LON;
+    @Input() titulo: string = TITULO ;
+   
+   
+    constructor(private mapService: LeafletService) {
+    }
+    ngOnInit(): void {
+      if (this.mapService.L) {
+          this.initMap();
+      }
+    }
+      
+   
+    private initMap(): void {
+      var iconRetinaUrl = 'assets/marker-icon-2x.png';
+      var iconUrl = 'assets/marker-icon.png';
+      var shadowUrl = 'assets/marker-shadow.png';
+      var iconDefault: any;
+      var iconDefault = this.mapService.L.icon({
         iconRetinaUrl,
         iconUrl,
         shadowUrl,
@@ -142,39 +157,50 @@ export class MapaComponent implements OnInit {
         tooltipAnchor: [16, -28],
         shadowSize: [41, 41]
       });
-     L.Marker.prototype.options.icon = iconDefault;
- 
-      //titulo
-      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://1938.com.es">Web Inteligencia Artificial</a>'
-      });
- 
-      //marca con pop up
-      const lon = this.lon + 0.009;
-      const lat = this.lat + 0.009;
-      const marker = L.marker([lat + 0.005, lon + 0.005]).bindPopup(this.titulo);
-      marker.addTo(this.map);
- 
-      //marca forma de circulo
-      const mark = L.circleMarker([this.lat, this.lon]).addTo(this.map);
-      mark.addTo(this.map);
- 
- 
-    //ruta
-    L.Routing.control({
-      router: L.Routing.osrmv1({
-        serviceUrl: `https://router.project-osrm.org/route/v1/`
-      }),
-      showAlternatives: true,
-      fitSelectedRoutes: false,
-      show: false,
-      routeWhileDragging: true,
-      waypoints: [
-        L.latLng(this.lat, this.lon),
-        L.latLng(lat, lon)
-      ]
-    }).addTo(this.map);
-      tiles.addTo(this.map);
-    }*/
-}
+        this.mapService.L.Marker.prototype.options.icon = iconDefault;
+   
+        this.map =  this.mapService.L.map('map', {
+          center: [this.lat, this.lon],
+          attributionControl: false,
+          zoom: 14
+        });
+   
+        const tiles = this.mapService.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="https://emergencias.dynamic-dns.net/">Web Inteligencia Artificial</a>'
+        });
+        //const marker = L.marker([this.lat, this.lon]);
+        //marker.addTo(this.map);
+   
+        const lon = this.lon + 0.009;
+        const lat = this.lat + 0.009;
+        const marker = this.mapService.L.marker([lat + 0.005, lon + 0.005]).bindPopup(this.titulo);
+        marker.addTo(this.map);
+   
+        const mark = this.mapService.L.circleMarker([this.lat, this.lon]).addTo(this.map);
+        mark.bindPopup(this.titulo);
+        mark.addTo(this.map);
+   
+       const mark2 = this.mapService.L.circleMarker([lat, lon]).addTo(this.map);
+        mark2.addTo(this.map);
+   
+   
+   
+      this.mapService.L.Routing.control({
+        router: this.mapService.L.Routing.osrmv1({
+          serviceUrl: `https://router.project-osrm.org/route/v1/`
+        }),
+        showAlternatives: true,
+        fitSelectedRoutes: false,
+        show: false,
+        routeWhileDragging: true,
+        waypoints: [
+          this.mapService.L.latLng(this.lat, this.lon),
+          this.mapService.L.latLng(lat, lon)
+        ]
+      }).addTo(this.map);
+   
+        tiles.addTo(this.map);
+   
+      }
+    }
